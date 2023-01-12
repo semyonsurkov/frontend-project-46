c15705335a7174cadcf23999fff99abc0f7b11f4
semyonsurkov <semyonsurkov@outlook.com>
2023-01-12 23:54:57 +0300

    Update gendiff.js [STEP2]

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
