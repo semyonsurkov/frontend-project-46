4070699cd4616432a440ec3ee26eecdb706963d8
semyonsurkov <semyonsurkov@outlook.com>
2023-02-06 16:18:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import { readFileSync } from 'node:fs';
 import { extname, resolve } from 'path';
-import parse from './parser.js';
+import process from 'process';
 import _ from 'lodash';
-
+import parse from './parser.js';
 
 const prepareData = (filepath) => {
   const type = extname(filepath).slice(1);
   const formedFilePath = resolve(process.cwd(), filepath);
-  const file = readFileSync(formedFilePath, 'utf-8')
+  const file = readFileSync(formedFilePath, 'utf-8');
   return parse(file, type);
-}
+};
 
 const genDiff = (filepath1, filepath2) => {
   const data1 = prepareData(filepath1);
@@ -22,17 +22,16 @@ const genDiff = (filepath1, filepath2) => {
 
   const result = sortUniqGeneralArray.map((key) => {
     if (!keysData2.includes(key)) {
-      return `- ${key}: ${data1[key]}`;
+      return `  - ${key}: ${data1[key]}`;
     }
     if (!keysData1.includes(key)) {
-      return `+ ${key}: ${data2[key]}`;
+      return `  + ${key}: ${data2[key]}`;
     }
     if (data1[key] === data2[key]) {
-      return `  ${key}: ${data1[key]}`;
+      return `    ${key}: ${data1[key]}`;
     }
-    return `- ${key}: ${data1[key]} \n+ ${key}: ${data2[key]}`;
+    return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
   });
-  return result.join('\n');
-  
+  return `{\n${result.join('\n')}\n}`;
 };
 export default genDiff;
