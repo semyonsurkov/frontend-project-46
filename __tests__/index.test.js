9372f64fa860ede95a8b72544670bb48c1dfc20e
Semyon Surkov <semyonsurkov@outlook.com>
2023-02-13 16:39:33 +0300

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,12 +10,14 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFixtureFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const cases = ['json', 'yml', 'yaml'];
-const expectedOutput = readFixtureFile('rightjson.txt');
+const expectedStylish = readFixtureFile('rightjson.txt');
+const expectedPlain = readFixtureFile('rightplain.txt');
 
 describe('genDiff', () => {
   test.each(cases)('Compare format %s', (format) => {
     const filepath1 = getFixturePath(`file1.${format}`);
     const filepath2 = getFixturePath(`file2.${format}`);
-    expect(genDiff(filepath1, filepath2)).toBe(expectedOutput);
+    expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
+    expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
   });
 });
