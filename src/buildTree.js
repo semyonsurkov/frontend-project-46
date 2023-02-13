5aaa33bd34a46dc1c90757bb73ad20d348c58d53
Semyon Surkov <semyonsurkov@outlook.com>
2023-02-13 14:57:19 +0300

    Update buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,48 +1,50 @@
 import _ from 'lodash';
 
-const buildTree = (data1, data2) => {
-  const keys2 = Object.keys(data1);
-  const keys1 = Object.keys(data2);
-  const keys = _.sortBy(_.union(keys1, keys2));
+const isObject = (value) => typeof value === 'object' && value !== null && !Array.isArray(value);
+
+const getSortedUniqueKeys = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  return _.sortBy(_.union(keys1, keys2));
+};
+
+const buildTree = ({ data1, data2 }) => {
+  const keys = getSortedUniqueKeys(data1, data2);
   return keys.map((key) => {
-    if (!_.has(data2, key)) {
-      return {
-        key,
-        type: 'deleted',
-        value: data1[key],
-      };
+    switch (true) {
+      case !_.has(data2, key):
+        return {
+          key,
+          type: 'deleted',
+          value: data1[key],
+        };
+      case !_.has(data1, key):
+        return {
+          key,
+          type: 'added',
+          value: data2[key],
+        };
+      case isObject(data1[key]) && isObject(data2[key]):
+        return {
+          key,
+          type: 'nested',
+          children: buildTree({ data1: data1[key], data2: data2[key] }),
+        };
+      case !_.isEqual(data1[key], data2[key]):
+        return {
+          key,
+          type: 'changed',
+          value1: data1[key],
+          value2: data2[key],
+        };
+      default:
+        return {
+          key,
+          type: 'unchanged',
+          value: data1[key],
+        };
     }
-
-    if (!_.has(data1, key)) {
-      return {
-        key,
-        type: 'added',
-        value: data2[key],
-      };
-    }
-
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return {
-        key,
-        type: 'nested',
-        children: buildTree(data1[key], data2[key]),
-      };
-    }
-
-    if (!_.isEqual(data1[key], data2[key])) {
-      return {
-        key,
-        type: 'changed',
-        value1: data1[key],
-        value2: data2[key],
-      };
-    }
-
-    return {
-      key,
-      type: 'unchanged',
-      value: data1[key],
-    };
   });
 };
+
 export default buildTree;
