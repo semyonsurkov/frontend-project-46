7a7bfb1759a6c7a6a91ab471bc512f9ccf9888e2
Semyon Surkov <semyonsurkov@outlook.com>
2023-02-13 16:41:30 +0300

    Update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -12,37 +12,36 @@ const stringify = (value) => {
 
 const getPath = (parentNames, name) => [...parentNames, name].join('.');
 
-const iter = (data, parentNames = []) =>
-  data
-    .filter((node) => node.type !== 'unchanged')
-    .flatMap((node) => {
-      switch (node.type) {
-        case 'added': {
-          const { key, value } = node;
-          return `Property '${getPath(
-            parentNames,
-            key
-          )}' was added with value: ${stringify(value)}`;
-        }
-        case 'deleted': {
-          const { key } = node;
-          return `Property '${getPath(parentNames, key)}' was removed`;
-        }
-        case 'changed': {
-          const { key, value1, value2 } = node;
-          return `Property '${getPath(
-            parentNames,
-            key
-          )}' was updated. From ${stringify(value1)} to ${stringify(value2)}`;
-        }
-        case 'nested': {
-          const { key, children } = node;
-          return iter(children, [getPath(parentNames, key)]);
-        }
-        default:
-          throw new Error(`Node type ${node.type} is not defined`);
+const iter = (data, parentNames = []) => data
+  .filter((node) => node.type !== 'unchanged')
+  .flatMap((node) => {
+    switch (node.type) {
+      case 'added': {
+        const { key, value } = node;
+        return `Property '${getPath(
+          parentNames,
+          key,
+        )}' was added with value: ${stringify(value)}`;
       }
-    });
+      case 'deleted': {
+        const { key } = node;
+        return `Property '${getPath(parentNames, key)}' was removed`;
+      }
+      case 'changed': {
+        const { key, value1, value2 } = node;
+        return `Property '${getPath(
+          parentNames,
+          key,
+        )}' was updated. From ${stringify(value1)} to ${stringify(value2)}`;
+      }
+      case 'nested': {
+        const { key, children } = node;
+        return iter(children, [getPath(parentNames, key)]);
+      }
+      default:
+        throw new Error(`Node type ${node.type} is not defined`);
+    }
+  });
 
 const formatToPlain = (tree) => iter(tree).join('\n');
 
