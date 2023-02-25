44adacc53bd6818d9d9c61a149170ced9cc642a4
Semyon Surkov <semyonsurkov@outlook.com>
2023-02-25 09:54:15 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,17 +6,20 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 const readFixtureFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const cases = ['json', 'yml', 'yaml'];
-const expectedStylish = readFixtureFile('rightjson.txt');
-const expectedPlain = readFixtureFile('rightplain.txt');
+const expectedStylish = readFixtureFile('rightStylish.txt');
+const expectedPlain = readFixtureFile('rightPlain.txt');
 
 describe('genDiff', () => {
   test.each(cases)('Compare format %s', (format) => {
     const filepath1 = getFixturePath(`file1.${format}`);
     const filepath2 = getFixturePath(`file2.${format}`);
+    expect(genDiff(filepath1, filepath2)).toEqual(expectedStylish);
     expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedStylish);
     expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedPlain);
   });
